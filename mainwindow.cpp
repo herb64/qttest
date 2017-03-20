26adbdd596a543e0e1a1e414f1a1be2407e1b824
Herbert Mehlhose <herbert.mehlhose@arcor.de>
2017-03-20 11:31:23 +0100

    commit for main window with test text

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "unistd.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    char text[80];
+    strcpy(text, "Klick mich, ");
+    ui->pushButton->setText(strcat(text, getlogin()));
+    // beim klick actions...
 }
 
 MainWindow::~MainWindow()
